2425e5aeadc3200e0192bca3c87b83f134a705d4
Sibiao Liu <sibiao89@gmail.com>
2021-04-21 13:53:14 +0200

    Merge from upstream

---
+++ ASPECT_build/include/aspect/config.h
@@ -0,0 +1,36 @@
+/*
+  Copyright (C) 2020 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _aspect_config_h
+#define _aspect_config_h
+
+#define ASPECT_SOURCE_DIR "/Users/sliu/GoogleDrive/1_Research/Aspect/aspect_July21"
+#define ASPECT_USE_SHARED_LIBS 1
+
+// optional features:
+/* #undef ASPECT_HAVE_LINK_H */
+/* #undef ASPECT_WITH_LIBDAP */
+/* #undef ASPECT_WITH_NETCDF */
+/* #undef ASPECT_WITH_PERPLEX */
+#define ASPECT_WITH_WORLD_BUILDER
+/* #undef ASPECT_USE_FP_EXCEPTIONS */
+/* #undef ASPECT_USE_PETSC */
+
+#endif
