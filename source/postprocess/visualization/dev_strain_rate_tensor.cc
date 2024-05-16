03b5a4b3fbf1830b2d82342848fc51f6badb3630
Sibiao Liu <sibiao89@gmail.com>
2024-05-16 13:58:29 +0200

    create the deviatoric strain rate tensor and deviatoric stress tensor outputs

---
+++ source/postprocess/visualization/dev_strain_rate_tensor.cc
@@ -0,0 +1,127 @@
+/*
+  Copyright (C) 2011 - 2022 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/postprocess/visualization/dev_strain_rate_tensor.h>
+
+
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      template <int dim>
+      DevStrainRateTensor<dim>::
+      DevStrainRateTensor ()
+        :
+        DataPostprocessorTensor<dim> ("dev_strain_rate_tensor",
+                                      update_gradients | update_quadrature_points),
+        Interface<dim>("1/s")
+      {}
+
+
+
+      template <int dim>
+      void
+      DevStrainRateTensor<dim>::
+      evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
+                            std::vector<Vector<double>> &computed_quantities) const
+      {
+        const unsigned int n_quadrature_points = input_data.solution_values.size();
+        Assert (computed_quantities.size() == n_quadrature_points, ExcInternalError());
+        Assert ((computed_quantities[0].size() == Tensor<2,dim>::n_independent_components),
+                ExcInternalError());
+        Assert (input_data.solution_gradients[0].size() == this->introspection().n_components, ExcInternalError());
+        
+        // If the material model is prescribed dike injection
+        if (this->get_parameters().enable_dike_injection)
+          {
+            MaterialModel::MaterialModelInputs<dim> in(input_data,
+                                                       this->introspection());
+            MaterialModel::MaterialModelOutputs<dim> out(n_quadrature_points,
+                                                         this->n_compositional_fields());
+
+            this->get_material_model().create_additional_named_outputs(out);
+            this->get_material_model().evaluate(in, out);
+
+            MaterialModel::PrescribedPlasticDilation<dim>
+            *prescribed_dilation = out.template get_additional_output<MaterialModel::PrescribedPlasticDilation<dim> >();
+
+            for (unsigned int q=0; q<n_quadrature_points; ++q)
+              {
+                SymmetricTensor<2,dim> strain_rate = in.strain_rate[q];
+                //Only keep the effect of dilation term on purely horizontal component
+                strain_rate[0][0] -= prescribed_dilation->dilation[q];
+
+                const SymmetricTensor<2,dim> deviatoric_strain_rate
+                  = strain_rate - 1./3 * trace(strain_rate) * unit_symmetric_tensor<dim>();
+
+                for (unsigned int d=0; d<dim; ++d)
+                  for (unsigned int e=0; e<dim; ++e)
+                    computed_quantities[q][Tensor<2,dim>::component_to_unrolled_index(TableIndices<2>(d,e))]
+                      = deviatoric_strain_rate[d][e];
+              }           
+          }
+        else
+          {
+            for (unsigned int q=0; q<n_quadrature_points; ++q)
+              {
+                Tensor<2,dim> grad_u;
+                for (unsigned int d=0; d<dim; ++d)
+                  grad_u[d] = input_data.solution_gradients[q][d];
+
+                const SymmetricTensor<2,dim> strain_rate = symmetrize(grad_u);
+                const Tensor<2,dim> deviatoric_strain_rate
+                  = strain_rate - 1./3 * trace(strain_rate) * unit_symmetric_tensor<dim>();
+
+                for (unsigned int d=0; d<dim; ++d)
+                  for (unsigned int e=0; e<dim; ++e)
+                    computed_quantities[q][Tensor<2,dim>::component_to_unrolled_index(TableIndices<2>(d,e))]
+                      = deviatoric_strain_rate[d][e];
+              }
+          }
+
+        const auto &viz = this->get_postprocess_manager().template get_matching_postprocessor<Postprocess::Visualization<dim>>();
+        if (!viz.output_pointwise_stress_and_strain())
+          average_quantities(computed_quantities);
+
+      }
+    }
+  }
+}
+
+
+// explicit instantiations
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      ASPECT_REGISTER_VISUALIZATION_POSTPROCESSOR(DevStrainRateTensor,
+                                                  "deviatoric strain rate tensor",
+                                                  "A visualization output object that generates output "
+                                                  "for the 4 (in 2d) or 9 (in 3d) components of the deviatoric strain rate "
+                                                  "tensor, i.e., for the components of the tensor "
+                                                  "Physical units: \\si{\\per\\second}.")
+    }
+  }
+}
